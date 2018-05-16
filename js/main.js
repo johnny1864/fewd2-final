a3b956eccd1e39b914eb4117827f3d2d6125d84b
johnny1864 <johnny1864@yahoo.com>
2018-05-15 21:30:21 -0700

    added validation

--- js/main.js
+++ js/main.js
@@ -19,5 +19,18 @@ window.addEventListener('load', function () {
         $('my-modal').style.display = 'none';
     });
 
-
+    $('submit-address').addEventListener('click', function(){
+        var inputs = document.querySelectorAll('#info-form input');
+        validation(inputs);
+    });
 });
+
+function validation(inputs){
+    console.log(inputs[0])
+    for(var i=0; i<inputs.length; i++){
+        if(inputs[i].value === '' && inputs[i].id !=='other'){
+            console.log('enter input');
+            
+        }
+    }
+}
