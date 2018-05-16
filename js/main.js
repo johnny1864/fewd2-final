577c0a33302af96d3dba169c5a5b94241659f240
johnny1864 <johnny1864@yahoo.com>
2018-05-15 20:37:56 -0700

    finished modal

---
+++ js/main.js
@@ -0,0 +1,23 @@
+/*eslint-env browser*/
+
+//HELPER FUNCTION
+var $ = function (id) {
+    'use strict';
+    return document.getElementById(id);
+};
+
+window.addEventListener('load', function () {
+    'use strict';
+
+    //DISPLAYS MODAL
+    $('order').addEventListener('click', function () {
+        $('my-modal').style.display = 'flex';
+    });
+
+    //CLOSES MODAL
+    $('close').addEventListener('click', function () {
+        $('my-modal').style.display = 'none';
+    });
+
+
+});
