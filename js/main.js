2cbd746c3058557a1075f893cfdc83e43a02c1e9
johnny1864 <johnny1864@yahoo.com>
2018-05-29 21:11:40 -0700

    added inputs to billing card

--- js/main.js
+++ js/main.js
@@ -8,6 +8,18 @@ var $ = function (id) {
 
 window.addEventListener('load', function () {
     'use strict';
+    
+    var addressType = $('inputAddress-type');
+
+    //DISPLAY OTHER INPUT
+    addressType.addEventListener('change', function(){
+        
+        if(addressType.value === 'other'){
+            $('inputOther').style.visibility = 'visible';
+        }else {
+            $('inputOther').style.visibility = 'hidden';
+        }
+    })
 
     //DISPLAYS MODAL
     $('order').addEventListener('click', function () {
@@ -19,19 +31,20 @@ window.addEventListener('load', function () {
         $('my-modal').style.display = 'none';
     });
 
+
     $('submit-address').addEventListener('click', function (e) {
         var inputs = document.querySelectorAll('#info-form');
         if (!validation(inputs)) {
             e.preventDefault()
         } else {
-            storeAddress(inputs)    
+            storeAddress(inputs)
         }
     });
 });
 
 //VALIDATES ADDRESS INPUTS 
 function validation(inputs) {
-    //console.log(inputs[0][3])
+
     var valid = true;
     for (var i = 0; i < inputs[0].length; i++) {
         if (inputs[0][i].value === '' && inputs[0][i].id !== 'other') {
@@ -48,10 +61,10 @@ function validation(inputs) {
 //STORES ADDRESS TO LOCAL STORAGE
 function storeAddress(inputs) {
     var inputArray = [];
-    console.log(inputs[0]);
+
     for (var i = 1; i < inputs[0].length; i++) {
         inputArray.push(inputs[0][i].value);
     }
-    
+
     sessionStorage.setItem('address', JSON.stringify(inputArray));
 }
