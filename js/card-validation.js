75dc6a457fb02391a240b614d228483d47a5040f
johnny <johnny1864@yahoo.com>
2018-06-05 21:09:14 -0700

    finished luhn function

--- js/card-validation.js
+++ js/card-validation.js
@@ -16,15 +16,20 @@ window.addEventListener('load', function () {
     $('submit-order').addEventListener('click', function (e) {
         e.preventDefault();
 
-        validateCardNumber();
-        validateDate();
-
+        cardNumberValidate();
+        dateValidate();
+        luhnValidation();
+        
+    });
+    
+    $('credit').addEventListener('keydown', function(){
+       cardType(); 
     });
 
     main();
 });
 
-function validateCardNumber() {
+function cardNumberValidate() {
     'use strict';
     var cardNumber = $('credit').value;
     cardNumber = parseInt(cardNumber, 10);
@@ -38,16 +43,66 @@ function validateCardNumber() {
     return true;
 
 }
-    
-function validateDate() {
+
+function dateValidate() {
     'use strict';
-    
+
     var currentDate = new Date();
     var currentYear = currentDate.getFullYear();
     var currentMonth = currentDate.getMonth();
-    
-    var expirationDate = $('ex-date');
-    
-    
-    
+    var inputDate = $('ex-date').value;
+
+
+
 }
+
+function luhnValidation() {
+    'use strict';
+
+    var sum = 0;
+    var temp = 0;
+    var tempArr = [];
+    var cardNumber = $('credit').value;
+
+    cardNumber = cardNumber.split('').reverse();
+    
+    for (var i = 0; i<cardNumber.length; i++){
+        temp = (+cardNumber[i])*2;
+        //console.log(temp);
+        if(i%2 !== 0){
+            //console.log(temp);
+            if(temp >= 10){
+                tempArr = temp.toString().split('');
+                //console.log(tempArr);
+                sum += +tempArr[0];
+                sum += +tempArr[1];
+            }else{
+                sum += temp;
+            }
+        }else{
+            sum += (+cardNumber[i]);
+        }
+    }
+    
+    if(sum/10 === 0){
+      return true;  
+    }
+    
+    return false;
+}
+
+//4512113014843252
+function cardType(){
+    'use strict';
+    
+    var cardNumber = $('credit').value;
+    
+    if(cardNumber[0] === '4'){
+        $('type').innerText = 'Visa';
+    }else if(cardNumber[0] === '5'){
+        $('type').innerText = 'MasterCard';
+    }else if(cardNumber[0] === '3'){
+        $('type').innerText = 'American Express';
+    }
+}
+
